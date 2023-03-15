815797c0715fb58573ca91d9dc0a4ac25c1f10b6
mhenriette <mhenriette00@gnmail.com>
2023-03-15 14:59:43 +0200

    refactor: format code with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
-import Calculator from './components/Calculator'
+import "./App.css";
+import Calculator from "./components/Calculator";
 
 const App = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-[#bdc3c7] to-[#2c3e50]">
       <Calculator />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 
-const Button = ({ children, className, onClick, large, clicked, index, isDisabled }) => {
-
-    return <>
-        <button
-            className={` h-20 border-gray-500 border text-center w-full ${className} ${large && 'col-span-2'} ${((clicked === index) && clicked !== undefined) && 'bg-indigo-100 text-orange-800'} `}
-            onClick={onClick} disabled={isDisabled && children === '.' ? true : false}
-        >{children}
-        </button>
-    </>;
+const Button = ({
+  children,
+  className,
+  onClick,
+  large,
+  clicked,
+  index,
+  isDisabled,
+}) => {
+  return (
+    <>
+      <button
+        className={` h-20 border-gray-500 border text-center w-full ${className} ${
+          large && "col-span-2"
+        } ${
+          clicked === index &&
+          clicked !== undefined &&
+          "bg-indigo-100 text-orange-800"
+        } `}
+        onClick={onClick}
+        disabled={isDisabled && children === "." ? true : false}
+      >
+        {children}
+      </button>
+    </>
+  );
 };
 
 export default Button;
